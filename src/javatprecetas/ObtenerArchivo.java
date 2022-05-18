4a7106c6aad1049e786c1e66791e44a1973895bf
JSierra <JSierra@NBK143.coa.local>
2022-05-18 20:43:24 -0300

    Corrección clase obtenerArchivo y Receta

--- src/javatprecetas/ObtenerArchivo.java
+++ src/javatprecetas/ObtenerArchivo.java
@@ -4,10 +4,27 @@
  */
 package javatprecetas;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author JSierra
  */
 public class ObtenerArchivo {
+    private BufferedReader csvRecetas = null;
+    private BufferedReader csvIngredientes = null;
     
+    //Contructor        
+    public void ObtenerArchivo(String pathRecetas, String pathIngredientes){        
+        try {
+            csvRecetas = new BufferedReader(new FileReader(pathRecetas));
+            csvIngredientes = new BufferedReader(new FileReader(pathIngredientes));
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(ObtenerArchivo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
--- src/javatprecetas/Receta.java
+++ src/javatprecetas/Receta.java
@@ -30,3 +30,4 @@ public class Receta {
     class getListaIngrediente { public getListaIngrediente() { 
         }
     }
+}
