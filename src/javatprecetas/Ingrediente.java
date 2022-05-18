b090a0681322947f45e64a971813e2ef6adfef17
luissanchezvilchez <99820267+luissanchezvilchez@users.noreply.github.com>
2022-05-18 01:42:56 -0300

    actualizacion de ingredientes - terminado

--- src/javatprecetas/Ingrediente.java
+++ src/javatprecetas/Ingrediente.java
@@ -8,12 +8,27 @@ package javatprecetas;
  *
  * @author VTCLUSAR
  */
-public class Ingrediente {
-    private Integer codigo;
 
-    public Integer getCodigo() {
-        return codigo;
+public class Ingrediente {
+    
+    String codigo, nombre;
+    int cantidad;
+    
+    public Ingrediente(String codigo, String nombre, int cantidad) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.cantidad = cantidad;
     }
-    
-    
+
+       //setter
+    public void setCodigo(String codigo) { this.codigo = codigo; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public void setCantidad(int cantidad) { this.cantidad = cantidad; }
+
+    //getter
+    public String getCodigo() { return codigo; }
+    public String getNombre() { return nombre; }
+    public int getCantidad() { return cantidad; }
+       
 }
+
