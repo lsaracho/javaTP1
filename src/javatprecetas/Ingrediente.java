f9d415b92d0385eb25719b7e94102fe8c89f017b
Chino <88289548+ruddy-crespo@users.noreply.github.com>
2022-05-27 19:20:13 -0300

    Update Ingrediente.java

--- src/javatprecetas/Ingrediente.java
+++ src/javatprecetas/Ingrediente.java
@@ -5,27 +5,38 @@
 package javatprecetas;
 
 /**
- *
+ * Declaracion de la clase Ingredientes
  * @author VTCLUSAR
  */
 
 public class Ingrediente {
-    
+    /**
+    * Declaracion de los atributos privados
+    */
     private String codigo, nombre;
     private int cantidad;
-    
+    /**
+    * Creacion del Constructor
+    * @param codigo
+    * @param nombre
+    * @param cantidad
+   */
     public Ingrediente(String codigo, String nombre, int cantidad) {
         this.codigo = codigo;
         this.nombre = nombre;
         this.cantidad = cantidad;
     }
 
-       //setter
+    /**
+    * Metodo para establecer formato de ingredientes en su respectivo formato
+    */
     public void setCodigo(String codigo) { this.codigo = codigo; }
     public void setNombre(String nombre) { this.nombre = nombre; }
     public void setCantidad(int cantidad) { this.cantidad = cantidad; }
 
-    //getter
+    /**
+    * Metodo para obtener cada ingrediente en su respectivo formato
+    */
     public String getCodigo() { return codigo; }
     public String getNombre() { return nombre; }
     public int getCantidad() { return cantidad; }
