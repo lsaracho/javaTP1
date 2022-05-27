cf66765a3b244084c56b6814366030092a9e46ee
Chino <88289548+ruddy-crespo@users.noreply.github.com>
2022-05-27 19:23:24 -0300

    Update Receta.java

--- src/javatprecetas/Receta.java
+++ src/javatprecetas/Receta.java
@@ -6,28 +6,46 @@ package javatprecetas;
 
 import java.util.ArrayList;
 /**
- *
+ * Declaracion de la clase Receta.
+ * Contiene informacion de la Receta.
  * @author VTCLUSAR
  */
 public class Receta {
+    /**
+     * Declaracion de los atributos privados
+     */
     private String nombre;
     private ArrayList<Ingrediente> listaIngrediente;
-
+    /**
+    * Creacion del constructor
+    * @param nombre
+    */
     public Receta(String nombre) {
         this.nombre = nombre;
     }  
         
-   
+   /**
+    * Metodo para obtener el nombre de la receta
+    * @return nombre
+    */
     public String getNombre() { 
         return nombre; 
     }
-    
+    /**
+    * Metodo para obtener la lista de ingredientes mediante un ArrayList
+    * @return listaIngredientes
+    */
     public ArrayList<Ingrediente> getListaIngrediente() { 
         return listaIngrediente; 
     }
-  
+    /**
+    * Metodo para establecer el nombre de la receta
+    * @param nombre
+    */
     public void setNombre(String nombre) { this.nombre = nombre; }
-    
+    /**
+    * Metodo para establecer la lista de ingredientes mediante un ArrayList
+    */
     public void setListaIngrediente(ArrayList<Ingrediente> listaIngrediente) {
         this.listaIngrediente = listaIngrediente; }
   
